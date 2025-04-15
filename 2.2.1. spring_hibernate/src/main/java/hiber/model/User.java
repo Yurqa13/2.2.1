cffaa7e89d983d8e62918c7c090bb64fd8ede542
Yurqa13 <138401033+Yurqa13@users.noreply.github.com>
2025-04-15 19:56:03 +0300

    Добавлен equals и hashCode

--- 2.2.1. spring_hibernate/src/main/java/hiber/model/User.java
+++ 2.2.1. spring_hibernate/src/main/java/hiber/model/User.java
@@ -1,6 +1,7 @@
 package hiber.model;
 
 import javax.persistence.*;
+import java.util.Objects;
 
 @Entity
 @Table(name = "users")
@@ -77,6 +78,21 @@ public class User {
               ", email='" + email + '\'' +
               ", car=" + car +
               '}';
+   }
+
+   @Override
+   public boolean equals(Object o) {
+      if (this == o) return true;
+      if (o == null || getClass() != o.getClass()) return false;
+      User user = (User) o;
+      return Objects.equals(email, user.email)
+              && Objects.equals(firstName, user.firstName)
+              && Objects.equals(lastName, user.lastName);
+   }
+
+   @Override
+   public int hashCode() {
+      return Objects.hash(email, firstName, lastName);
    }
 
 }
